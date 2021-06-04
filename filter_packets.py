bbb6eb246e9583f1c0656c379258a64d70fe38fb
Sohom Sen <sohom416@hotmail.com>
2021-06-03 23:58:10 -0400

    Revert "Code cleanup"
    This reverts commit 3e4e2790a97b6fc8e0407c5e9505be258a670937.

--- filter_packets.py
+++ filter_packets.py
@@ -1,20 +1,19 @@
 import re
 
-
 def filter(filename):
     regex = r"(?s)\b(?:(?!\n\n).)*?\b(ping)\b(?:(?!\n\nNo\.).)*"
 
     data = open(filename).read()
-
+    
     matches = re.finditer(regex, data, re.MULTILINE)
-
+    
     filename_split = filename.split('.')
     filename_join = '_filtered.'.join(filename_split)
 
-    out_file = open(filename_join, 'w')
-
+    out_file = open(filename_join,'w')
+    
     for match in matches:
-        line = str(match.group(0)) + '\n\n'
+        line = str(match.group(0))+'\n\n'
         out_file.write(line)
 
     out_file.close()
